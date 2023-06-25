f30729f1c49eec573be08c932adcc80ec04e9815
Ryan Williams <ryanjacobwilliams@gmail.com>
2023-06-25 17:54:32 -0500

    chore: update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./containers/App";
 import "tachyons";
-// import registerServiceWorker from './registerServiceWorker';
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
